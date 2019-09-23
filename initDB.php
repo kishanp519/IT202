9bd47c556e014b01dc0d06e9216faa6b35a54030
kishanp519 <kishanp519@gmail.com>
2019-09-23 12:32:53 -0400

    Fix

--- initDB.php
+++ initDB.php
@@ -9,35 +9,27 @@ echo $host;
 $connection_string = "mysql:host=$host;dbname=$database;username=$username;password=$password;charset=utf8mb4";
 
 try {
+
+	foreach(glob("sql/*.sql") as $filename){
+		//echo $filename;
+		$sql[$filename] = file_get_contents($filename);
+		//echo $sql[$filename];
+	}
+	ksort($sql);
+
 	$db = new PDO($connection_string, $username, $password);
 	echo " - Database should have connected.";
-         
-       $query = "create table if not exists `TestUsers` (
-						`id` int auto_increment not null, 
-						`username` varchar(30) not null unique, 
-						`pin` int default 0, 
-						PRIMARY KEY (`id`)
-						) CHARACTER SET utf8 COLLATE utf8_general_ci";
-	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
-	$stmt = $db->prepare($query);
-	$r = $stmt->execute();
-	echo "<br>" . ($r>0?"Created table or already exists.":"Failed to create table.") . "<br>";
-	unset($r);
-	$insert_query = "INSERT INTO `TestUsers`(`username`, `pin`) VALUES (:username, :pin)";
-	$stmt = $db->prepare($insert_query);
-	$newUser = "Billy";
-	$newPin = 1234;
-	$r = $stmt->execute(array(":username"=> $newUser, ":pin"=> $newPin));
-
-	print_r($stmt->errorInfo());
-
-	echo "<br>" . ($r>0?"Insert successful":"Insert failed") . "<br>";
-
-	$select_query = "select * from `TestUsers` where username = :username";
-	$stmt = $db->prepare($select_query);
-	$r = $stmt->execute(array(":username"=> "Billy"));
-	$results = $stmt->fetch(PDO::FETCH_ASSOC);
-	echo "<pre>" . var_export($results, true) . "</pre>";
+        //$db->setAttribute(PDO::ATTR_ERRMODE);
+	foreach($sql as $key => $value){
+		echo "<br>Running: " . $key;
+		$stmt = $db->prepare($value);
+		$result = $stmt->execute();
+		$error = $stmt->errorInfo();
+		if($error && $error[0] !== '00000'){
+			echo "<br>Error:<pre>" . var_export($error,true) . "</pre><br>";
+		}
+		echo "<br>$key result: " . ($result>0?"Success":"Fail") . "<br>";
+	}
 
 } catch (Exception $e) {
 	echo $e->getMessage();
