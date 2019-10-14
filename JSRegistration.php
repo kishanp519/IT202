8f069ae88a8936533c79687bc37d3bae91d44105
kishanp519 <kishanp519@gmail.com>
2019-10-14 10:30:56 -0400

    HW Addition

---
+++ JSRegistration.php
@@ -0,0 +1,70 @@
+<html>
+<head>
+<script>
+function myValidation(inputEle, checkValue) {
+	let name = inputEle.name;
+	let vid = "validation." + name;
+        let vele = document.getElementById(vid);
+	let value = inputEle.value;
+
+	if (inputEle.type == "email") {
+		if (name == "email" || name == "confirmemail") {
+			if (value == "" || !value.includes("@") || !value.includes(".")) {
+				vele.document.createElement("span");
+				vele.id = vid;
+				document.body.appendChild(vele);
+			}
+			vele.innerText = name + " has an invalid email value.";
+		}
+		if (name == "confirmemail" && value != checkValue.value) {
+			vele.document.createElement("span");
+                        vele.id = vid;
+                        document.body.appendChild(vele);	
+		}
+                vele.innerText = name + " has an invalid email value.";
+	}
+
+	if (inputEle.type == "password") {
+ 		if (name == "password" || name == "passwordconfirm") {
+			if (value == "") {
+				vele.document.createElement("span");
+				vele.id = vid;
+				document.body.appendChild(vele);
+			}
+			vele.innterText = name + " has an invalid password value.";			
+		}	 
+		if (name == "passwordconfirm" && value != checkValue.value) {
+                        vele.document.createElement("span");
+                        vele.id = vid;
+                        document.body.appendChild(vele);
+        	}
+	}
+
+	if (name == "username") {
+		if (value == "") {
+			if (value == "") {
+				vele.document.createElement("span");
+				vele.id = vid;
+				document.body.appendChild(vele);
+			}
+			vele.innerText = name + " has an invalid username value."
+		}
+	}
+
+	return false;
+}
+</script>
+</head>
+
+<body>
+<form onsubmit="return false;">
+<input name="email" type="email" onchange="myValidation(this, this);"/>
+<input name="emailconfirm" type="email" onchange="myValidation(this, email);"/>
+<input name="password" type="password" onchange="myValidation(this, this);"/>
+<input name="passwordconfirm" type="password" onchange="myValidation(this, this);"/>
+<input name="username" onchange="myValidation(this, this);"/>
+
+<input type="submit" value "Submit"/>
+</form>
+</body>
+</html>
