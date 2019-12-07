43936c4b0ff32ec1bed73885b35a306eb2aa4a02
kishanp519 <kishanp519@gmail.com>
2019-12-07 16:46:52 -0500

    Almost done

--- PharmacyManagement/home.php
+++ PharmacyManagement/home.php
@@ -13,6 +13,27 @@ function getPharmacyUsers($pharmacy) {
 	$r = $stmt->execute();
 	return $stmt->fetchAll();
 }
+
+function getPharmacyPatients($pharmacy) {
+	require("config.php");
+	$conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
+	$db = new PDO($conn_string, $username, $password);
+	$query = "SELECT name from PharmacyPatientData WHERE pharmacy=:pharmacy";
+	$stmt = $db->prepare($query);
+	$stmt->bindValue(":pharmacy", $pharmacy);
+	$r = $stmt->execute();
+	return $stmt->fetchAll();
+}
+
+function getMedications(){
+	require("config.php");
+	$conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
+	$db = new PDO($conn_string, $username, $password);
+	$query = "SELECT DISTINCT name, strength, daySupply from PharmacyMedicationData";
+	$stmt = $db->prepare($query);
+	$r = $stmt->execute();
+	return $stmt->fetchAll();
+}
 ?>
 
 <head>
@@ -94,11 +115,34 @@ if (isset($_POST['selectedUser']) && isset($_POST['selectedRole'])) {
 </section>
 
 <section>
+<center> <font color="black"> <b> Patient Panel </b> </font> </center>
+<font color="black"> <b> New Patient: </b> </font>
+<br style = line-height:100px;>
+
+<form method="POST">
+	First Name:<input name="firstName" type="text" placeholder="Enter first name"/>
+	Last Name:<input name="lastName" type="text" placeholder="Enter last name"/>
+	Age:<input name="age" type ="number" placeholder="Enter age"/>
+	<input type="submit" value="Execute"/>
+</form>
+<?php
+if (isset($_POST['firstName']) && isset($_POST['lastName']) && isset($_POST['age'])) {
+        $first = $_POST['firstName'];
+	$last = $_POST['lastName'];
+	$age = $_POST['age'];
+	createPatient($first, $last, $age, get_pharmacy());
+	echo "You have created a new patient with the inputted fields.";
+} 
+?>
+<b> <hr> </b>
+</section>
+
+<section>
 <center> <font color="black"> <b> Medication Panel </b> </font> </center>
 <font color="black"> <b> New Medication: </b> </font>
 <br style = line-height:100px;>
 
-<form method="post">
+<form method="POST">
 	Name:<input name="name" type="text" placeholder="Enter name"/>
 	Strength:<input name="strength" type="text" placeholder="Enter strength"/>
 	Day Supply:<input name="daySupply" type ="number" placeholder="Enter day supply"/>
@@ -115,6 +159,53 @@ if (isset($_POST['name']) && isset($_POST['strength']) && isset($_POST['daySuppl
 ?>
 <b> <hr> </b>
 </section>
+
+<section>
+<center> <font color="black"> <b> Prescription Panel </b> </font> </center>
+<font color="black"> <b> New Prescription: </b> </font>
+<br style = line-height:100px;>
+<font color="black"> Choose a Patient: </font>
+
+<?php
+$patients = getPharmacyPatients(get_pharmacy());
+$medications = getMedications();
+?>
+
+<form method="POST">
+
+<select name="patientID">
+<?php foreach($patients as $index=>$row):?>
+	<option value="<?php echo $row['id'];?>">
+		<?php echo "Name: " . $row['firstName'] . " " . $row['lastName'] . ", Age: " . $row['age'];?>
+	</option>
+<?php endforeach;?>
+</select>
+
+<br style = line-height:100px;>
+<font color="black"> Choose a Medication: </font>
+
+<select name="medicationID">
+<?php foreach($medication as $index=>$row):?>
+	<option value="<?php echo $row['id'];?>">
+		<?php echo "Name: " . $row['name'] . " Strength: " . $row['strength'] . ", Day Supply: " . $row['daySupply'];?>
+	</option>
+<?php endforeach;?>
+</select>
+
+<br style = line-height:100px;>
+<font color="black"> Write Instructions: </font>
+Name:<input name="instructions" type="text" placeholder="Enter instructions"/>
+<input type="submit" value="Execute"/>
+</form>
+<?php
+if (isset($_POST['patientID']) && isset($_POST['medicationID']) && isset($_POST['instructions'])) {
+        $patientID = $_POST['patientID'];
+	$medicationID = $_POST['medicationID'];
+	createPrescription($patientID, $medicationID, $instructions);
+	echo "You have created a prescription for the patient with the provided medication.";
+} 
+?>
+</section>	
 
 </body>
 </html>
